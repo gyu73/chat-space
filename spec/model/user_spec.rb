c363ea5ff4aa9ce594f02ee7f3e943faa22c580d
Masahiro Yagyu <gyu7372@gmail.com>
2017-03-15 12:28:37 +0900

    メールがからの場合のテスト実行

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -9,5 +9,11 @@ describe User do
      expect(user.errors[:nickname]).to include("を入力してください。")
     end
 
+   it "is invalid without a email" do
+     user = build(:user, email: "")
+     user.valid?
+     expect(user.errors[:email]).to include("を入力してください。")
+    end
+
   end
 end

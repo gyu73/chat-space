fcc465c5d3285a554a8a010bc6c6be846f70d193
Masahiro Yagyu <gyu7372@gmail.com>
2017-03-15 23:29:19 +0900

    chat_groupsコントローラー　chat_groupモデル　chat_groupsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170315142521) do
+ActiveRecord::Schema.define(version: 20170315142805) do
+
+  create_table "chat_groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"

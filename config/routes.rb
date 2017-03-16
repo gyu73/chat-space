56681cc766507bf7a8a10943f0c9369eb2e766e8
Masahiro Yagyu <gyu7372@gmail.com>
2017-03-16 16:30:29 +0900

    %記法の追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'chat_groups#index'
-  resources :chat_groups ,only: [:index,:new,:edit] do
-    resources :message , only: [:index]
+  resources :chat_groups ,only: %i(index new edit) do
+    resources :message , only: %i(index)
       end
 end

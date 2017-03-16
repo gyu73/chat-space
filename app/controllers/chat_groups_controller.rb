32733ae1816af48a88ba2333be622bd3b627c3e3
Masahiro Yagyu <gyu7372@gmail.com>
2017-03-17 00:50:13 +0900

    コントローラーにcreateアクション追加。　ルーティングにcreate追加

--- app/controllers/chat_groups_controller.rb
+++ app/controllers/chat_groups_controller.rb
@@ -3,9 +3,19 @@ class ChatGroupsController < ApplicationController
   end
 
   def new
+    @group = ChatGroup.new
+  end
+
+  def create
+    ChatGroup.create(create_params)
+    redirect_to action: :index
   end
 
   def edit
   end
 
-end
+  private
+    def create_params
+      params.require(:chat_group).permit(:name)
+    end
+  end

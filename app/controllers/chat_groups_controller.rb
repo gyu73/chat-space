d5aec8e7190ffffa951c0dfcaac8c435996a89b2
Masahiro Yagyu <gyu7372@gmail.com>
2017-03-21 15:50:43 +0900

    重複している記述のメソッド化

--- app/controllers/chat_groups_controller.rb
+++ app/controllers/chat_groups_controller.rb
@@ -8,17 +8,19 @@ class ChatGroupsController < ApplicationController
   end
 
   def create
-    @group = ChatGroup.create(chat_group_params)
+    group = ChatGroup.new(chat_group_params)
+    group.save
     # add_user_id_to_relation_table(chat_group)
-    redirect_to chat_group_messages_path(@group.id)
+    redirect_to chat_group_messages_path(group)
+    binding.pry
   end
 
   def edit
-    @group = ChatGroup.find(params[:id])
+    @group = get_params_id
   end
 
   def update
-    current_group = ChatGroup.find(params[:id])
+    current_group = get_params_id
     current_group.update(chat_group_params)
     redirect_to chat_group_messages_path(current_group.id)
   end
@@ -28,17 +30,22 @@ class ChatGroupsController < ApplicationController
       params.require(:chat_group).permit(:name , user_ids: [])
     end
 
-  def add_user_id_to_relation_table(chat_group)
-     x = 1
-    next_while = true
-    while next_while do
-        if x <= chat_group_params[:user_ids].count-1
-          users = User.find(chat_group_params[:user_ids][x])
-          chat_group.users << users
-          x = x + 1
-        else
-          next_while = false
-        end
+    def get_params_id
+    ChatGroup.find(params[:id])
     end
-  end
+
+  # def add_user_id_to_relation_table(chat_group)
+  #    x = 1
+  #   next_while = true
+  #   while next_while do
+  #       if x <= chat_group_params[:user_ids].count-1
+  #         users = User.find(chat_group_params[:user_ids][x])
+  #         chat_group.users << users
+  #         x = x + 1
+  #       else
+  #         next_while = false
+  #       end
+  #   end
+  # end
+
 end

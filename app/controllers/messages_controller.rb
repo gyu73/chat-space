0ab132a835e188cb2b87baac9f7431d9cee00a0b
Masahiro Yagyu <gyu7372@gmail.com>
2017-03-21 15:50:16 +0900

    インデント修正、不要な記述削除、ハッシュスペース

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,5 @@ class MessagesController < ApplicationController
     @current_group = ChatGroup.find(params[:chat_group_id])
     @current_group_members = @current_group.users
     @current_user_groups = current_user.chat_groups.order('created_at DESC')
-    binding.pry
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'chat_groups#index'
   resources :chat_groups ,only: %i(index new create edit update) do
     resources :messages , only: %i(index)
-      end
+  end
 end

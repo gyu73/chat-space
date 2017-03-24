26dcc1a3867904baeba410c73c2ebf714f833042
Masahiro Yagyu <gyu7372@gmail.com>
2017-03-24 19:33:41 +0900

    送信したデータを保存するcreateアクションを作成し、indexアクションにメッセージのインスタンス変数を作成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,27 @@ class MessagesController < ApplicationController
     @current_group = ChatGroup.find(params[:chat_group_id])
     @current_group_members = @current_group.users
     @current_user_groups = current_user.chat_groups.order('created_at DESC')
-    @message = Message.new
+    @messages = @current_group.messages.includes(:user)
+    @new_message = Message.new
   end
+
+  def create
+    message = Message.new(content: message_params[:content], user_id: current_user.id, chat_group_id: chat_group_params[:chat_group_id])
+    group = ChatGroup.new(id: chat_group_params[:chat_group_id])
+    if message.save
+      redirect_to chat_group_messages_url(group), notice: 'メッセージを送信しました'
+    else
+      redirect_to chat_group_messages_url(group), alert: 'メッセージの送信に失敗しました。'
+    end
+  end
+end
+
+private
+
+def message_params
+  params.require(:message).permit(:content)
+end
+
+def chat_group_params
+  params.permit(:chat_group_id)
 end

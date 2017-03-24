bbac3c6db3246f5aa0be07e65c19db14f9b2eca5
Masahiro Yagyu <gyu7372@gmail.com>
2017-03-24 21:04:09 +0900

    bulletを用いてN+1問題解決

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   def index
     @current_group = ChatGroup.find(params[:chat_group_id])
     @current_group_members = @current_group.users
-    @current_user_groups = current_user.chat_groups.order('created_at DESC')
+    @current_user_groups = current_user.chat_groups.order('created_at DESC').includes(:user_chat_groups).includes(:users)
     @messages = @current_group.messages.includes(:user)
     @new_message = Message.new
   end

4c286e2a0fed14f9ad5506c99ce68748f8c2edfa
Masahiro Yagyu <gyu7372@gmail.com>
2017-03-25 12:05:30 +0900

    インデント修正

--- spec/model/message_spec.rb
+++ spec/model/message_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 describe Message do
-    describe '#create' do
-      it "is valid with a content, image, user_id, chat_group_id" do
-        message = build(:message)
-        expect(message).to be_valid
+  describe '#create' do
+    it "is valid with a content, image, user_id, chat_group_id" do
+      message = build(:message)
+      expect(message).to be_valid
     end
 
-      it "is invalid without a content" do
-        user = build(:message, content: nil)
-        user.valid?
-        expect(user.errors[:content]).to include("を入力してください。")
-      end
+    it "is invalid without a content" do
+      user = build(:message, content: nil)
+      user.valid?
+      expect(user.errors[:content]).to include("を入力してください。")
+    end
   end
 end

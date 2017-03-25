d151684632ede7eedb81415747b6fa74dc4fdac4
Masahiro Yagyu <gyu7372@gmail.com>
2017-03-25 11:52:15 +0900

    messageモデルのテスト作成
    メッセージが空の場合送信できないようにできているか確認。
    メッセージが入っている場合に送信できるかの確認。

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,6 +1,10 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
+
   factory :message do
+    content "aaa"
+    image { Faker::Avatar.image }
+    user_id {Faker::Number.number(2)}
+    chat_group_id {Faker::Number.number(2)}
   end
+
 end

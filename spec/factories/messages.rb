d3e48f546fadfe5b7bc41ac2a342fa75cb0b706a
Masahiro Yagyu <gyu7372@gmail.com>
2017-03-25 12:03:46 +0900

    contentもFakerでおいた

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
 
   factory :message do
-    content "aaa"
+    content { Faker::Zelda.character }
     image { Faker::Avatar.image }
     user_id {Faker::Number.number(2)}
     chat_group_id {Faker::Number.number(2)}

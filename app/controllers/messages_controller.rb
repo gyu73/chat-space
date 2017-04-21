a1251d7fae5a3c88f5dc612b8f350946c77571b6
Masahiro Yagyu <gyu7372@gmail.com>
2017-04-21 17:07:00 +0900

    create_API

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,7 +12,7 @@ class MessagesController < ApplicationController
     if message.save
       respond_to do |format|
         format.html { redirect_to chat_group_messages_url(message.chat_group_id), notice: 'メッセージを送信しました(あなたの思いが伝わりますように。)' }
-        format.json { render json: message.json}
+        format.json { render json: message.json }
       end
     else
       redirect_to chat_group_messages_url(message.chat_group_id), alert: 'メッセージの送信に失敗しました。(メッセージは225文字以下でお願いします。)'

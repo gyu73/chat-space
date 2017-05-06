ea64e4f9dcbbff2c75796add1112648ec82c192b
Masahiro Yagyu <gyu7372@gmail.com>
2017-05-06 12:17:55 +0900

    create_auto_upload_function square (num, num){ console.log(num * num) }

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function() {
   function buildHTML(chat_group) {
 
     var html =
-      `<div class= contents__right__center__message>
+      `<div class= contents__right__center__message data-id=${chat_group.message_id}>
           <h3 class= contents__right__center__message__name>
           ${chat_group.user_nickname}
           </h3>
@@ -14,6 +14,26 @@ $(function() {
           </h3>
           <div class= contents__right__center__message__image>
             <img src=${chat_group.image} >
+          </div>
+        </div>` ;
+    return html;
+  }
+
+  function updateHTML(message, nickname) {
+
+    var html =
+      `<div class= contents__right__center__message data-id=${message.id}>
+          <h3 class= contents__right__center__message__name>
+          ${nickname}
+          </h3>
+          <h3 class= contents__right__center__message__date>
+          ${message.created_at}
+          </h3>
+          <h3 class= contents__right__center__message__text>
+          ${message.content}
+          </h3>
+          <div class= contents__right__center__message__image>
+            <img src=${message.image.url} >
           </div>
         </div>` ;
     return html;
@@ -43,4 +63,34 @@ $(function() {
     e.preventDefault();
     SendAjax();
   });
+
+  setInterval(getNewMessages,10000);
+
+  function getNewMessages(){
+    var LastMessageId = $(".contents__right__center__message:last-child").data("id");
+    $.ajax({
+      type: "GET",
+      url: location.href,
+      data: {
+        LastMessageId: LastMessageId
+      },
+      dataType: "json"
+    })
+    .done(function(data){
+      if(data.length != 0){
+        var html = "";
+        $.each(data[1], function(index1, nickname){
+          $.each(data[0], function(index2, message){
+            html += updateHTML(message, nickname);
+          });
+        });
+        $('.contents__right__center').append(html);
+      }
+      else{
+      }
+    })
+    .fail(function(){
+    });
+  }
+
 });
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,6 +8,10 @@ before_action :authenticate_user!
     @current_user_groups = current_user.chat_groups.order('created_at DESC').includes(:user_chat_groups).includes(:users)
     @messages = @current_group.messages.includes(:user)
     @new_message = Message.new
+    respond_to do |format|
+      format.html
+      format.json {render json: get_create_json(get_update_messages, get_update_user_nicknames)}
+    end
   end
 
   def create
@@ -15,7 +19,7 @@ before_action :authenticate_user!
     if message.save
       respond_to do |format|
         format.html { redirect_to chat_group_messages_url(message.chat_group_id), notice: 'メッセージを送信しました(あなたの思いが伝わりますように。)' }
-        format.json { render json: message.json.merge(image: message.image.url) }
+        format.json { render json: message.create_json.merge(image: message.image.url, message_id: message.id) }
       end
     else
       redirect_to chat_group_messages_url(message.chat_group_id), alert: 'メッセージの送信に失敗しました。(メッセージは225文字以下でお願いします。)'
@@ -27,4 +31,25 @@ private
 
 def message_params
   params.require(:message).permit(:content, :image, :chat_group_id)
+end
+
+def get_update_messages
+    update_messages = ChatGroup.find(params[:chat_group_id]).messages.where('id = 582 ')
+end
+
+def get_update_user_nicknames
+    update_users = []
+    update_messages = ChatGroup.find(params[:chat_group_id]).messages.where('id = 582')
+    update_messages.each do |message|
+      user = User.find(message[:user_id]).nickname
+      update_users << user
+    end
+    return update_users
+end
+
+def get_create_json(get_update_messages, get_update_user_nicknames)
+  array = []
+  array = array.push(get_update_messages)
+  array = array.push(get_update_user_nicknames)
+  return array
 end

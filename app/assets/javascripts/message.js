7d5f20239e2be8320e2857400f1b8c2ad27903a9
Masahiro Yagyu <gyu7372@gmail.com>
2017-05-08 12:17:18 +0900

    work autoload function only in message index view

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,5 @@
 $(function() {
   function buildHTML(chat_group) {
-
     var html =
       `<div class= contents__right__center__message data-id= ${chat_group.message_id}>
           <h3 class= contents__right__center__message__name>
@@ -20,7 +19,6 @@ $(function() {
   }
 
   function updateHTML(message, nickname) {
-
     var html =
       `<div class= contents__right__center__message data-id=${message.id}>
           <h3 class= contents__right__center__message__name>
@@ -38,7 +36,7 @@ $(function() {
         </div>` ;
     return html;
   }
-
+ //メッセージの非同期通信のためのメソッド
   function SendAjax() {
     var text_data = new FormData($('#new_message').get(0));
     $.ajax({
@@ -57,39 +55,46 @@ $(function() {
       alert('メッセージを入力してください。');
     });
   }
-
 // 以下が送信ボタンが押された時に実行される
   $('.contents__right__bottom__form__send').on('click', function(e) {
     e.preventDefault();
     SendAjax();
   });
-
-if($(".contents").data("page") == "message_index") {
-  setInterval(getNewMessages,60000);
-}
-
+  //自動更新のメソッド
   function getNewMessages() {
-    var LastMessageId = $(".contents__right__center__message:last-child").data("id");
-    $.ajax( {
-      type: "GET",
-      url: location.href,
-      data: {
-        LastMessageId: LastMessageId
-      },
-      dataType: "json"
-    })
-    .done(function(data) {
-      if(data.length != 0) {
-        var html = "";
-        $.each(data[1], function(index1, nickname) {
-          $.each(data[0], function(index2, message) {
-            html += updateHTML(message, nickname);
+    var current_chat_group_id = $(".contents").data("chat-group-id");
+    if(location.pathname == `/chat_groups/${current_chat_group_id}/messages`) {
+      var LastMessageId = $(".contents__right__center__message:last-child").data("id");
+      $.ajax( {
+        type: "GET",
+        url: location.href,
+        data: {
+          LastMessageId: LastMessageId
+        },
+        dataType: "json"
+      })
+      .done(function(data) {
+        if(data.length != 0) {
+          var html = "";
+          $.each(data[1], function(index1, nickname) {
+            $.each(data[0], function(index2, message) {
+              html += updateHTML(message, nickname);
+            });
           });
-        });
-        $('.contents__right__center').append(html);
-      }
-    })
-    .fail(function() {
-    });
+          $('.contents__right__center').append(html);
+        }
+      })
+      .fail(function() {
+        alert('メッセージを自動更新できませんでした。');
+      });
+    }
+  }
+  //自動更新のための記述
+  var current_chat_group_id = $(".contents").data("chat-group-id");
+  if(location.pathname == `/chat_groups/${current_chat_group_id}/messages`) {
+    var timer = setInterval(getNewMessages, 5000);
+  }
+  else {
+    clearInterval(timer);
   }
 });
